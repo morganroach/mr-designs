b98a179db4f7d5e93a63258ac990341186e116cb
morganroach <morganroach34@gmail.com>
2026-06-02 22:06:40 -0400

    Changes
    C

--- script.js
+++ script.js
@@ -1,7 +1,1 @@
-const form = document.querySelector("form");
-
-form.addEventListener("submit", function(event) {
-  event.preventDefault();
-  alert("Thanks! Your message was submitted.");
-  form.reset();
-});
+//Empty 
